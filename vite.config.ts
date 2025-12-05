f605f4bc327f1b20e234ddb6bff369b1dc68228c
Marcus König <marcus@spaghetti.ventures>
2025-12-05 16:42:10 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: './',  // VERY IMPORTANT FOR GITHUB PAGES
+  base: './',
   plugins: [react()],
   build: {
     outDir: 'docs',
     emptyOutDir: true,
-  }
+  },
 });
